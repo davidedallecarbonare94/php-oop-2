daddd64c7213b5fd408c1a90cdcf42957616f158
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-06-28 15:59:52 +0200

    add product instances

--- index.php
+++ index.php
@@ -11,9 +11,53 @@ oppure diverse tipologie di prodotti.
 Provate a far interagire tra di loro gli oggetti: 
 ad esempio, l'utente dello shop inserisce una carta di credito... */
 
+class Product{
+    public $name;
+    public $desc;
+    public $price;
+    public $genre;
 
 
+    function __construct(string $name, string $desc, int $price, string $genre){
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->price = $price;
+        $this->genre = $genre;
 
+    }
+
+
+}
+
+$pizza = new Product("Regina", "Pizza Regina", 2.99, "food");
+$seaSalad = new Product("Re del Mare", "Insalata di Mare", 5.99, "food");
+$speaker = new Product("JBL", "Bluetooth Speaker", 16.99, "hi-tec");
+$vape = new Product("Vape", "Ammazza Zanzare", 3.99, "life-saver");
+$bandAid = new Product("Band-Aid", "Cerotti primo soccorso", 4.49, "life-saver");
+$mixer = new Product("Parkside", "Tritatutto", 14.99, "hi-tec");
+
+//var_dump($pizza);
+
+class User{
+    public $firstName;
+    public $lastName;
+    public $age;
+
+
+    function __construct(string $firstName, string $lastName,  int $age){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->age = $age;
+
+    }
+
+    class Premium extends User{
+        function __construct($firstName, $lastName, $age, bool $premium){
+            parent::__construct($firstName, $lastName, $age);
+            $this->premium = $premium;
+        }
+    }
+}
 
 
 ?>
